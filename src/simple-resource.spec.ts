d6ff4ad918f61a42117842aa5b858d790d61ec50
Ilia Pozdnyakov <iliazeus@proton.me>
2023-07-04 01:21:37 +0600

    set up the project

---
+++ src/simple-resource.spec.ts
@@ -0,0 +1,54 @@
+import "disposablestack/auto";
+import { assert } from "chai";
+
+const openHandles = new Set<number>();
+
+function openHandle(): number {
+  const handle = Math.random();
+  openHandles.add(handle);
+  return handle;
+}
+
+function closeHandle(handle: number): void {
+  if (!openHandles.has(handle)) throw new Error("invalid handle");
+  openHandles.delete(handle);
+}
+
+class SimpleResource {
+  #handle: number;
+
+  constructor() {
+    this.#handle = openHandle();
+  }
+
+  #isDisposed = false;
+
+  dispose(): void {
+    if (this.#isDisposed) return;
+    this.#isDisposed = true;
+
+    closeHandle(this.#handle);
+  }
+
+  [Symbol.dispose](): void {
+    this.dispose();
+  }
+
+  get handle(): number {
+    if (this.#isDisposed) throw new Error("object is disposed");
+    return this.#handle;
+  }
+}
+
+describe("SimpleResource", () => {
+  it("works with using", () => {
+    assert.isEmpty(openHandles);
+
+    {
+      using resource = new SimpleResource();
+      assert.doesNotThrow(() => resource.handle);
+    }
+
+    assert.isEmpty(openHandles);
+  });
+});
